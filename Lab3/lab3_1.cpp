b78c6d67f49d9b6b839d0f3244aa158cef46c121
Distman <sittawit15@gmail.com>
2026-07-11 22:32:55 +0700

    update code

--- Lab3/lab3_1.cpp
+++ Lab3/lab3_1.cpp
@@ -4,15 +4,10 @@ using namespace std;
 
 int main()
 {
-    double f, c;
-    cout << "Enter f : ";
-    cin >> f;
-    c = (5.0 / 9) * (f - 32);
-    cout << 5.0 / 9 << endl;
-    cout << "f = " << f << endl;
-    cout << "c = " << c << endl;
-    f = ((9.0 / 5) * c) + 32.0;
-    cout << "f = " << f << endl;
-    cout << "c = " << c << endl;
+    cout << "Result of 3/2 +5 = " << (3 / 2) + 5 << endl;
+    cout << "Result of 5/1 +2 = " << (5 / 1) + 2 << endl;
+    cout << "Result of 3*2 + 4*5 = " << (3 * 2) + 4 * 5 << endl;
+    cout << "Result of 4-2 + 5/3+2 = " << (4 - 2) + 5 / 3 + 2 << endl;
+    cout << "Result of 10-2 + 7%2-1 = " << (10 - 2) + 7 % 2 - 1 << endl;
     return (0);
 }
